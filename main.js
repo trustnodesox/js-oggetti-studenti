8b668e1929c7c77fa723b7554bf3bf966b8b9a3a
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-16 17:44:07 +0100

    stampo

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 //creo l'oggetto studente
 var studente = {
   nome: 'Lorenzo',
-  cognome: ['De sossi'],
+  cognome: 'De sossi',
   età: '19',
 }
 
 var studente2 = {
   nome: 'Michele',
-  cognome: ['Pippo'],
+  cognome: 'Pippo',
   età: '18',
 }
 
 var studente3 = {
   nome: 'Antonio',
-  cognome: ['Pluto'],
+  cognome: 'Pluto',
   età: '22',
 }
 
@@ -24,3 +24,24 @@ for (var key in studente) {
 }
 
 //creo array di oggetti
+var classe = [studente, studente2, studente3];
+console.log(classe);
+
+//stampo nome e cognome di tutti gli studenti
+for (var i = 0; i < classe.length; i++) {
+  console.log(classe[i].nome + ' ' + classe[i].cognome)
+}
+
+
+$('#invio').click(function () {
+  //var nome = $("#nome").val();
+  //var cognome = $("#cognome").val();
+  //var data = $("#data").val();
+  var studente4 = {
+    nome: $("#nome"),
+    cognome: $("#cognome"),
+    età: $("#data"),
+  }
+  classe.push(nome + cognome + data);
+  console.log(classe);
+})
